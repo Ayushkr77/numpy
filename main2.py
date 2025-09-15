813db42320a87853b0655d3511b6d58deb02e3d3
Ayush Kumar <ayushkumar5990@gmail.com>
2025-09-15 12:42:40 +0530

    Aggregate Functions

--- main2.py
+++ main2.py
@@ -9,28 +9,55 @@
 # Little problem, see bro code video and chatgpt
 
 
-import numpy as np
+# import numpy as np
 
-arr1=np.array([[1,2,3,4]])
-arr2=np.array([[1],[2],[3],[4]])
+# arr1=np.array([[1,2,3,4]])
+# arr2=np.array([[1],[2],[3],[4]])
 
-print(arr1.shape)
-print(arr2.shape)
+# print(arr1.shape)
+# print(arr2.shape)
 
-print(arr1*arr2)
+# print(arr1*arr2)
 
 
-# Exercise
+# # Exercise
 
-array1=np.array([[1,2,3,4,5,6,7,8,9,10]])
-array2=np.array([[1],[2],[3],[4],[5],[6],[7],[8],[9],[10]])
+# array1=np.array([[1,2,3,4,5,6,7,8,9,10]])
+# array2=np.array([[1],[2],[3],[4],[5],[6],[7],[8],[9],[10]])
 
-# print(array1.shape)
-# print(array2.shape)
+# # print(array1.shape)
+# # print(array2.shape)
 
-print(array1*array2)
-print(array1+array2)
+# print(array1*array2)
+# print(array1+array2)
 
 
 
 # -------------------------------------------------------------------------------------------------
+
+
+
+# Aggregate functions = summarize data and typically return a single value
+
+import numpy as np
+
+array = np.array([[1, 2, 3, 4, 5],
+                [6, 7, 8, 9, 10]])
+
+print(np.sum(array)) # Sum of all elements. or do the sum of inside arrays like sum(array[0])
+print(np.mean(array)) # Average of all elements
+print(np.std(array)) # Measure of standard deviation
+print(np.var(array)) # Variance, Square of standard deviation
+print(np.min(array)) # Smallest value
+print(np.max(array)) # Largest value
+print(np.argmin(array)) # Position (flattened) of smallest
+print(np.argmax(array)) # Position (flattened) of largest
+
+print(np.sum(array, axis=0)) # sum column-wise (vertically)
+print(np.sum(array, axis=1)) # sum row-wise (horizontally)
+
+
+
+# ---------------------------------------------------------------------------------------------------
+
+
